21b091f73b359b5a23ad3cc1704f4afea10f6d9d
RockZombie123 <91627371+RockZombie123@users.noreply.github.com>
2021-11-17 18:26:10 -0800

    Update apps

--- apps.sh
+++ apps.sh
@@ -9,7 +9,8 @@ MENU="Choose one of the following applications:"
 
 OPTIONS=(1 "Terminal"
          2 "Music Player"
-         3 "ASCII Video Player")
+	 3 "Update"
+         4 "ASCII Video Player")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -30,8 +31,14 @@ case $CHOICE in
             echo "You chose Option 2"
 	    python3 musicplayer.py
             ;;
-        3)
-            echo "You chose Option 3"
+
+	3)
+	   echo "Downloading from: https://github.com/RockZombie123/PythonProjects.git"
+	   git clone https://github.com/RockZombie123/PythonProjects.git
+	   ;;
+
+        4)
+            echo "You chose Option 4"
 	    python3 asciivideo.py
             ;;
 esac
